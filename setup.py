a5b217e7f524ba3b7dfc9a0110842c3c7850f4ab
Luke Blevins <lukeblevins@outlook.com>
2026-05-15 22:07:38 -0400

    increment package version

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 setup(
     name='poligrapher',
+    version='0.1.0',
     author='UCI Networking Group',
     include_package_data=True,
     packages=['poligrapher', 'poligrapher.annotators', 'poligrapher.scripts'],
